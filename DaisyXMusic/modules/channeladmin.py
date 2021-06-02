bb3cba4d6be38bddd03ca7060cd365824f4fea8a
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-06-02 17:18:04 +0530

    Update channeladmin.py

--- DaisyXMusic/modules/channeladmin.py
+++ DaisyXMusic/modules/channeladmin.py
@@ -28,7 +28,7 @@ from DaisyXMusic.services.callsmusic import callsmusic
 
 
 
-@Client.on_message(filters.command(["channelpause","cpause"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["2channelpause","2cpause"]) & filters.group & ~filters.edited)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
@@ -49,7 +49,7 @@ async def pause(_, message: Message):
         await message.reply_text("▶️ Paused!")
 
 
-@Client.on_message(filters.command(["channelresume","cresume"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["2channelresume","2cresume"]) & filters.group & ~filters.edited)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
@@ -70,7 +70,7 @@ async def resume(_, message: Message):
         await message.reply_text("⏸ Resumed!")
 
 
-@Client.on_message(filters.command(["channelend","cend"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["2channelend","2cend"]) & filters.group & ~filters.edited)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
@@ -94,7 +94,7 @@ async def stop(_, message: Message):
         await message.reply_text("❌ Stopped streaming!")
 
 
-@Client.on_message(filters.command(["channelskip","cskip"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["2channelskip","2cskip"]) & filters.group & ~filters.edited)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
@@ -127,7 +127,7 @@ async def skip(_, message: Message):
     await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
 
 
-@Client.on_message(filters.command("channeladmincache"))
+@Client.on_message(filters.command("2channeladmincache"))
 @errors
 async def admincache(client, message: Message):
     try:
