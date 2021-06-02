dde58b3edd31c4b93e6ca288cffb34068e0b8061
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-06-02 17:10:17 +0530

    Update pmpermit.py

--- DaisyXMusic/modules/pmpermit.py
+++ DaisyXMusic/modules/pmpermit.py
@@ -33,8 +33,8 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
-            )
+                "**Welcome To DaisyX Music Assistant**\n\n**🛑 Rules:**\n1. Don't Spam Here. \n2. No Chatting Here.\n\n**⚠️ Disclamer:** If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
+           ) 
             return
 
     
